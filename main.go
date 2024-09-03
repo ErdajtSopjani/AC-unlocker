afe3a9fb6524a6636fdb91f5705189ce6bdefc6f
Erdajt Sopjani <120386306+ErdajtSopjani@users.noreply.github.com>
2024-09-03 21:46:25 +0200

    fixed path on windows machines - now the path is converted with the correct slashes for the system it runs on - and added /content to the default path
    - added collider file in the repo for more context

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
-	"io"
 	"os"
+	"path/filepath"
 )
+
+//go:embed collider.kn5
+var colliderData []byte
 
 func main() {
 	fmt.Print("Assetto Corsa DLC Unlocker\nCode on: github.com/ErdajtSopjani/AC-unlocker\n\n")
 
-	fmt.Print("Default root folder: 'C:/Program Files (x86)/Steam/steamapps/common/assettocorsa'\n(leave empty to use the default one)\n\n")
+	fmt.Print("Default root folder: 'C:/Program Files (x86)/Steam/steamapps/common/assettocorsa/content'\n(leave empty to use the default one)\n\n")
 
 	fmt.Println("Enter the file path: ")
 	var path string
@@ -17,47 +21,53 @@ func main() {
 	// get the root folder from the user
 	fmt.Scanln(&path)
 	if path == "" { // go with the default one if the user input is empty
-		// C:/Program Files (x86)/Steam/steamapps/common/assettocorsa
-		path = "C:/Program Files (x86)/Steam/steamapps/common/assettocorsa"
+		path = "C:/Program Files (x86)/Steam/steamapps/common/assettocorsa/content"
 	}
+
+	// convert slashes to appropriate system format
+	path = filepath.FromSlash(path)
+
+	// cleanup path
+	path, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Println("Error resolving the path:", err)
+		return
+	}
+
 	fmt.Println("\nRoot folder set to: ", path)
 
 	// get all the folders in the cars folder
 	fmt.Println("\nReading files in the cars folder...")
-	folders, err := os.ReadDir(path + "/cars")
+	folders, err := os.ReadDir(filepath.Join(path, "cars"))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	errMessage := "Make sure the game is closed and the file path is correct.\n\nIf the problem presits report it on github.com/ErdajtSopjani/AC-unlocker/issues\n\n"
+	errMessage := "Make sure the game is closed and the file path is correct.\n\nIf the problem persists report it on github.com/ErdajtSopjani/AC-unlocker/issues\n\n"
+
 	fmt.Print("\nDoing the magic...\n")
-	// iterate through all the folders while reading the collider.kn5 files
+
+	// iterate through all folders while copying collider data
 	for _, folder := range folders {
 		if folder.IsDir() {
-			colliderFile, err := os.Open("./collider.kn5")
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer colliderFile.Close() // This should stay here to close the file after the loop ends
-
-			newColliderFile, err := os.Create(path + "/cars/" + folder.Name() + "/collider.kn5")
+			// create new collider file in directory
+			newColliderFile, err := os.Create(filepath.Join(path, "cars", folder.Name(), "collider.kn5"))
 			if err != nil {
 				fmt.Print("Something went wrong while creating the new collider file.\n", errMessage)
-				colliderFile.Close()
 				return
 			}
 			defer newColliderFile.Close()
 
-			_, err = io.Copy(newColliderFile, colliderFile)
+			// write the embedded collider data to the new file
+			_, err = newColliderFile.Write(colliderData)
 			if err != nil {
-				fmt.Print("Something went wrong while copying the collider file.\n", errMessage)
+				fmt.Print("Something went wrong while writing the collider file.\n", errMessage)
 				return
 			}
 		}
 	}
 
-	fmt.Println("Done, enjoy :)")
+	fmt.Println("Done, enjoy :)\n\np.s give it a star")
 	fmt.Scanln()
 }
